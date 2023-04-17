51212448da86ad6632db081de0930e838cd8f9f9
sarvesh018 <77659605+sarvesh018@users.noreply.github.com>
2023-04-17 21:57:30 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        boolean slots[] = new boolean[n];
+        Arrays.fill(slots, false);
+        int proft = 0, count = 0;
+        
+        Arrays.sort(arr, (a,b) -> b.profit-a.profit);
+        
+        for(int i=0; i<n; i++){
+            for(int j=arr[i].deadline-1; j>=0; j--){
+                if(slots[j] == false){
+                    count++;
+                    proft += arr[i].profit;
+                    slots[j] = true;
+                    break;
+                }
+            }
+        }
+        return new int[]{count, proft};
+    }
+}
