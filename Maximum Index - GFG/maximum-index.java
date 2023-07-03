b0c36bcb2e3fa774518a2cc9a6d6db2f76f1434d
Sarvesh Choudhary <sarveshc111@gmail.com>
2023-07-03 22:22:35 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.java
+++ Maximum Index - GFG/maximum-index.java
@@ -22,22 +22,36 @@ class GFG {
 // } Driver Code Ends
 
 
-class Solution {
+class Solution 
+{    int maxIndexDiff(int arr[], int n) 
+     {  int[] leftMin = new int[n];
+        int[] rightMax = new int[n];
 
-    int maxIndexDiff(int arr[], int n) {
-        // code here
-        int max = 0;
-        int i=0, j=n-1;
-        while(i<j){
-            if(arr[i]<=arr[j]){
-                max = Math.max(j-i, max);
-                i++;
-                j = n-1;
+        leftMin[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(arr[i], leftMin[i - 1]);
+        }
+
+        rightMax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) 
+      {
+            rightMax[i] = Math.max(arr[i], rightMax[i + 1]);
+        }
+
+        int maxDiff = -1;
+        int i = 0, j = 0;
+        while (j < n && i < n) 
+       {
+            if (leftMin[i] <= rightMax[j]) 
+             {
+                maxDiff = Math.max(maxDiff, j - i);
+                j++;
             }
-            else{
-                j--;
+        else {
+                i++;
             }
         }
-        return max;
+
+        return maxDiff;
     }
 }
